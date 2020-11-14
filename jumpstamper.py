015a2806a44ca5b9681c91946f06faa78e311bb3
LJ Wobker <lwobker@lwobker-vms.rabidwombat.net>
2020-11-13 19:42:28 -0500

    refactor settings/profiles into separate class

--- jumpstamper.py
+++ jumpstamper.py
@@ -11,179 +11,6 @@ import math
 
 
 
-def jumpParser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i",   "--input_file", action="store", type=str, help="input video file")
-    parser.add_argument("-o",   "--output_file", action="store", type=str,  help="output video file")
-    parser.add_argument("-s",   "--stamp", action="store_true",  help="enable frame stamping")
-    parser.add_argument("-st",  "--slate_time", action="store", type=float,   help="duration of the slate frame")
-    parser.add_argument("-lt",  "--leadin_time", action="store", type=float,   help="start jump portion of video N seconds before exit")
-    parser.add_argument("-wt",  "--working_time", action="store", type=float,   help="seconds of working (scoring) time")
-    parser.add_argument("-jt",  "--jump_time", action="store", type=float,   help="length of the jump video")
-    parser.add_argument("-ef",  "--exit_frame", action="store", type=int, help="frame number of the exit")
-    parser.add_argument("-sf",  "--slate_frame", action="store", type=int,   help="frame number of the slate, 0 to disable")
-    parser.add_argument("-ft",  "--freeze_time", action="store", type=float,   help="duration of the freeze, 0 to disable")
-    parser.add_argument("-ovr", "--overlay_prof", action="store", type=str,   help="the overlay profile name")
-    parser.add_argument("-enc", "--encoder_prof", action="store", type=str,   help="the encoder options profile name")
-    parser.add_argument("-an",  "--annotation", action="store", type=str,   help="annotation string")
-    parser.add_argument("-xlsx", "--excel_sheet", action="store", type=str, help="parse excel: header row required with arguments, each row is one jump")
-
-    return parser
-
-
-
-def getVideoMetadata(infile: str) -> dict:
-    ''' returns a dict of metadata via ffprobe for the first VIDEO stream in input file. '''
-
-    try:
-        probe = ffmpeg.probe(infile) 
-    except ffmpeg.Error as e:
-        print(e.stderr, file=sys.stderr)
-        sys.exit(1)
-
-    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-    if video_stream is None:
-        print('No video stream found', file=sys.stderr)
-        sys.exit(1)
-
-    return video_stream
-
-
-def getFrameRate(vid_metadata: dict) -> float:
-    ''' returns a float with the probed frame rate from the video file. '''
-    rate = vid_metadata['r_frame_rate'].split('/')
-    # is it represented as a single integer value?
-    if len(rate)==1:
-        return float(rate[0])
-    # or as a fraction (usually NTSC...)
-    if len(rate)==2:
-        return float(rate[0])/float(rate[1])
-    return -1
-
-
-def overlayProf(profile_id: str, vid_metadata: dict) -> dict:
-    '''
-    returns a set of parameters for the drawtext filter based on the profile ID provided.
-    A set of common parameters are copied to each specific text box, and then we need
-    to modify whatever is unique to that text box.
-    '''
-
-    def roundToMultOf(x, base=10):
-        return base * round(x/base)
-
-    framerate = getFrameRate(vid_metadata)
-    vid_h = vid_metadata['height']
-    vid_w = vid_metadata['width']
-
-    # our parameter profiles are a dictionary of dicts
-
-    common = {}    # https://ffmpeg.org/ffmpeg-filters.html#drawtext-1 
-    common['fontfile'] = '/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf'
-    common['rate'] = framerate
-    common['fontcolor'] = 'yellow'
-    common['fontsize'] = roundToMultOf(vid_h/12 , 8)
-    common['box'] = 1
-    common['boxcolor'] = 'black@0.5'
-    common['boxborderw'] = 3
-
-    # adjust parameters for the frame counter 
-    framectr = {
-        **common, 
-        'y' : (vid_h * 0.9), 
-        'x' : (vid_w * 0.2),
-        'text' : 'in: %{frame_num} @ %{pts}',
-        'start_number' : 0,
-        }
-
-    # set the location of the timestamp
-    timestamp = {
-        **common, 
-        'y' : 0,
-        'x' : 0,
-        }
-
-    annot_fontsize = roundToMultOf(vid_h/16 , 8)
-    annot = {
-        **common,
-        'fontsize': annot_fontsize,       # 1/16th of height rounded
-        'y': vid_h - annot_fontsize,      # offset by the height from the bottom
-        'x': 0,
-        }   
-
-    profile = { 
-        'framectr': framectr, 
-        'timestamp': timestamp, 
-        'annot': annot, 
-        }
-    return profile
-
-
-
-def encoderProf(profile_id: str, vid_metadata: dict) -> dict:
-    ''' return the encoder profile.  Each profile has parameters for
-    - the output encode options: codec, codec options, quality, etc.
-    - the FFMPEG general options such as "no audio" and "hide_banner"
-
-    Suggestion: when creating a new profile, just make a deepcopy() of the default 
-    one, and change/overwrite the options that you need to.  
-    '''
-
-    framerate = getFrameRate(vid_metadata)
-    vid_h = vid_metadata['height']
-    vid_w = vid_metadata['width']
-
-    output = {
-        'c:v': 'libx264',
-        # 'pix_fmt' : 'yuvj420p',
-        'crf': 27,
-        'preset': 'slow',
-        'an': None,
-        'y': None,
-        'hide_banner': None,
-        'format': 'mp4',
-        'benchmark': None,
-    }
-
-    scale = {'width': vid_w, 'height': vid_h}
-    fps = {'fps': framerate}
-
-
-    # outputs in 1080 @ 30fps
-    if (profile_id == '1080_30'):
-        scale = {'width': '-4', 'height': '1080'}
-        fps = {'fps': 30}
-
-    # quick - useful for iterative testing!
-    if (profile_id == 'quick'):
-        scale = {'width': '-4', 'height': '480'}
-        # fps = {'fps': 25}
-        output['crf']  = 30
-        output['preset']  = 'veryfast'
-
-    if (profile_id == 'x265_high'):
-        output['c:v'] = 'libx265'
-        output['preset'] = 'medium'
-        output['crf'] = '28'
-
-    # the null profile does NOT inherit anything so we have to replicate here...
-    # for debug/test only  ;-)
-    if (profile_id == 'null'):
-        output = {
-            'format': 'null',
-            'y': None,
-            'hide_banner': None,
-            'benchmark': None,
-        }
-
-
-    return_id = {
-        'output' : output,
-        'scale' : scale,
-        'fps': fps,
-    }
-
-    return return_id
-
 
 
 @dataclass 
@@ -246,7 +73,6 @@ class StamperArgs:
         self.frames = { k:self.numFrames(v) for k,v in self.secs.items() }
         self.sanity()
 
-
     def sanity(self):
         ''' run some sanity checks on the computed values '''
 
@@ -256,6 +82,190 @@ class StamperArgs:
             self.framenum['leadin'] = 0
             # the leadin duration is now the exit frame number comverted back to seconds
             self.secs['leadin'] = self.numSecs(self.framenum['exit'])
+
+
+
+@dataclass
+class StamperProfiles:
+    '''
+    the StamperProfile class contains the various settings (aka a "profile") for the various
+    arguments and settings that we use in the main script.  The basic idea here is that the
+    default settings are as portable as we can make them, and if people want to alter the various
+    filter settings, they can do so here.  
+    '''
+
+    args : StamperArgs                  # the input StamperArgs
+                                        # select which overlay profile: required
+
+    def roundToMultOf(x, base=10):
+        return base * round(x/base)
+
+    def __post_init__(self):
+
+        vid_metadata = getVideoMetadata(self.args.input_file)
+        framerate = getFrameRate(vid_metadata)
+        vid_h = vid_metadata['height']
+        vid_w = vid_metadata['width']
+
+
+        self.common_dt : dict = {                # shared drawtext args for reuse 
+            'fontfile' : '/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf',
+            'font' : 'Sans',
+            'rate' : framerate,
+            'fontcolor' : 'yellow',
+            'fontsize' : round(vid_metadata['height'] / 12 / 8) * 8,
+            'box' : 1,
+            'boxcolor' : 'black@0.5',
+            'boxborderw' : 2,
+        }
+
+
+        self.framectr_dt = {              # drawtext args for the frame counter
+            **self.common_dt, 
+            'y' : (vid_h * 0.9), 
+            'x' : (vid_w * 0.2),
+            'text' : 'in: %{frame_num} @ %{pts}',
+            'start_number' : 0,
+        }
+
+        self.slate_trim = {               # trim filter for the slate
+            'start_frame' : self.args.framenum['slate'],
+            'end_frame' : self.args.framenum['slate'] + 1,
+        }
+        
+        self.jump_trim = {                # trim filter for the main jump
+            'start_frame': self.args.framenum['leadin'],   
+            'end_frame': self.args.framenum['end'],
+        }
+
+        self.timer_dt = {                 # drawtext filter for timer
+            **self.common_dt, 
+            'y' : 0,
+            'x' : 0,
+        }
+
+        annot_fontsize = round(vid_h / 16 / 8) * 8
+        self.annot_dt = {                 # drawtext filter for annotation
+            **self.common_dt,
+            'fontsize': annot_fontsize,       # 1/16th of height rounded
+            'y': vid_h - annot_fontsize,      # offset by the height from the bottom
+            'x': 0,
+            'text': self.args.annotation,
+        }
+
+        self.freeze_loop : dict = {             # loop filter for freezeframe
+            # freeze starts here because we have to compensate for the 'trim'
+            'start': self.args.framenum['freeze'] - self.args.framenum['leadin'] + 1,
+            'loop': self.args.frames['freeze'],
+            'size': 1,
+        }
+        
+        fade_duration = 2
+        self.jump_fade : dict = {               # fade filter for main jump
+            'type' : 'out',
+            'start_time' : self.args.secs['leadin'] + self.args.secs['jump'] + self.args.secs['freeze'] - fade_duration,
+            'duration' : fade_duration,
+        }
+        
+        self.fps : dict = {                     
+            'fps' : getFrameRate(vid_metadata)                   # default to original value
+        }
+        
+        self.scale : dict = {
+            'width' : vid_w,
+            'height' : vid_h,
+        }
+
+        self.output : dict = {                  # output arguments
+            'c:v': 'libx264',
+            # 'pix_fmt' : 'yuvj420p',
+            'crf': 27,
+            'preset': 'slow',
+            'an': None,
+            'y': None,
+            'hide_banner': None,
+            'format': 'mp4',
+            'benchmark': None,
+        }
+
+           # outputs in 1080 @ 30fps
+        if (self.args.encoder_prof == '1080_30'):
+            self.scale = {'width': '-4', 'height': '1080'}
+            self.fps = {'fps': 30}
+
+        # quick - useful for iterative testing!
+        if (self.args.encoder_prof == 'quick'):
+            self.scale = {'width': '-4', 'height': '480'}
+            # fps = {'fps': 25}
+            self.output['crf']  = 30
+            self.output['preset']  = 'veryfast'
+
+        if (self.args.encoder_prof == 'x265_high'):
+            self.output['c:v'] = 'libx265'
+            self.output['preset'] = 'medium'
+            self.output['crf'] = '28'
+
+        # the null profile does NOT inherit anything so we have to replicate here...
+        # for debug/test only  ;-)
+        if (self.args.encoder_prof == 'null'):
+            self.output = {
+                'format': 'null',
+                'y': None,
+                'hide_banner': None,
+                'benchmark': None,
+            }
+
+
+
+
+
+def jumpParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i",   "--input_file", action="store", type=str, help="input video file")
+    parser.add_argument("-o",   "--output_file", action="store", type=str,  help="output video file")
+    parser.add_argument("-s",   "--stamp", action="store_true",  help="enable frame stamping")
+    parser.add_argument("-st",  "--slate_time", action="store", type=float,   help="duration of the slate frame")
+    parser.add_argument("-lt",  "--leadin_time", action="store", type=float,   help="start jump portion of video N seconds before exit")
+    parser.add_argument("-wt",  "--working_time", action="store", type=float,   help="seconds of working (scoring) time")
+    parser.add_argument("-jt",  "--jump_time", action="store", type=float,   help="length of the jump video")
+    parser.add_argument("-ef",  "--exit_frame", action="store", type=int, help="frame number of the exit")
+    parser.add_argument("-sf",  "--slate_frame", action="store", type=int,   help="frame number of the slate, 0 to disable")
+    parser.add_argument("-ft",  "--freeze_time", action="store", type=float,   help="duration of the freeze, 0 to disable")
+    parser.add_argument("-ovr", "--overlay_prof", action="store", type=str,   help="the overlay profile name")
+    parser.add_argument("-enc", "--encoder_prof", action="store", type=str,   help="the encoder options profile name")
+    parser.add_argument("-an",  "--annotation", action="store", type=str,   help="annotation string")
+    parser.add_argument("-xlsx", "--excel_sheet", action="store", type=str, help="parse excel: header row required with arguments, each row is one jump")
+
+    return parser
+
+
+def getVideoMetadata(infile: str) -> dict:
+    ''' returns a dict of metadata via ffprobe for the first VIDEO stream in input file. '''
+
+    try:
+        probe = ffmpeg.probe(infile) 
+    except ffmpeg.Error as e:
+        print(e.stderr, file=sys.stderr)
+        sys.exit(1)
+
+    video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+    if video_stream is None:
+        print('No video stream found', file=sys.stderr)
+        sys.exit(1)
+
+    return video_stream
+
+
+def getFrameRate(vid_metadata: dict) -> float:
+    ''' returns a float with the probed frame rate from the video file. '''
+    rate = vid_metadata['r_frame_rate'].split('/')
+    # is it represented as a single integer value?
+    if len(rate)==1:
+        return float(rate[0])
+    # or as a fraction (usually NTSC...)
+    if len(rate)==2:
+        return float(rate[0])/float(rate[1])
+    return -1
 
 
 
@@ -306,66 +316,41 @@ def makeSlate(args: StamperArgs):
 
 
 
-def makeJump(args: StamperArgs) -> ffmpeg.nodes.FilterableStream:
+def makeMainJump(args: StamperArgs) -> ffmpeg.nodes.FilterableStream:
 
-    overlay_profile = overlayProf(args.overlay_prof, args.vid_metadata)
-    timestamp_args = overlay_profile['timestamp']
-    annot_args = overlay_profile['annot']
-    annot_args['text'] = args.annotation
-
-    trim_args = {
-        'start_frame': args.framenum['leadin'],   
-        'end_frame': args.framenum['end'],
-    }
-
-    # shift the freeze frame, because we trimmed the original stream and all the frame numbers change
-    args.framenum['freeze'] = args.framenum['freeze'] - args.framenum['leadin'] + 1
-    freeze_loop_args = {
-        'loop': args.frames['freeze'],
-        'size': 1,
-        'start': args.framenum['freeze'],
-    }
-
-    fade_duration = 2
-    jump_fade_args = {
-        'type' : 'out',
-        'start_time' : args.secs['leadin'] + args.secs['jump'] + args.secs['freeze'] - fade_duration,
-        'duration' : fade_duration,
-    }
-
+    settings = StamperProfiles(args)
 
     if (args.working_time == 0):          # set text to empty string, which causes drawtext to do nothing
-        timestamp_args['text'] = ''         
+        settings.timer_dt['text'] = ''         
     else:
         # this is an expression that drawtext can evaluate to get the timestamp in the format 
         # that we want.  See 'eif' 'trunc' 'abs' functions and 't' variable in docs.
         base_EIF_calc = '%{eif:(trunc(t-LEADIN)):d:2}.%{eif:abs((1M*(t-LEADIN)-1M*trunc(t-LEADIN))/10000):d:2}'
-        timestamp_args['text'] = base_EIF_calc.replace('LEADIN', str(args.secs['leadin']))
+        settings.timer_dt['text'] = base_EIF_calc.replace('LEADIN', str(args.secs['leadin']))
 
-    jump = (
+    main_jump = (
         ffmpeg
         .input(args.input_file)
-        .trim(**trim_args)
+        .trim(**settings.jump_trim)
         .setpts('N/FRAME_RATE/TB')      # fixup PTS for the trimmed stream
         .crop(width='0.8*in_w', height='ih', x='0.1*in_w', y=0)
-        .filter('drawtext', **timestamp_args)
-        .filter('drawtext', **annot_args)
-        .filter('loop', **freeze_loop_args)  
+        .filter('drawtext', **settings.timer_dt)
+        .filter('drawtext', **settings.annot_dt)
+        .filter('loop', **settings.freeze_loop)  
         .setpts('N/FRAME_RATE/TB')      # fixup PTS for the looped frames
-        .filter('fade', **jump_fade_args)
+        .filter('fade', **settings.jump_fade)
     )
-    return jump
+    return main_jump
 
 
 def joinAndPostFilters(args: StamperArgs, to_concat: list) -> ffmpeg.nodes.FilterableStream:
-    enc_opts = encoderProf(args.encoder_prof, args.vid_metadata)
-    scale_args = enc_opts['scale']
-    fps_filter_args = enc_opts['fps']
+
+    settings = StamperProfiles(args)
     joined = (
         ffmpeg
         .concat(*to_concat, v=1, a=0)
-        .filter('scale', **scale_args)
-        # .filter('fps', **fps_filter_args)
+        .filter('scale', **settings.scale)
+        .filter('fps', **settings.fps)
         .node       # I still don't know what this does.. 
     )
     return joined
@@ -375,27 +360,27 @@ def joinAndPostFilters(args: StamperArgs, to_concat: list) -> ffmpeg.nodes.Filte
 
 def processJumps(list_of_args: list):
 
-    for args in list_of_args:
-        enc_opts = encoderProf(args.encoder_prof, args.vid_metadata)
-        output_args = enc_opts['output']
+    for cli_args in list_of_args:
 
-        if args.stamp:      # the stamp flag is set, so do that...
-            framestamped = makeStamped(args)
-            to_output = joinAndPostFilters(args, [framestamped])
+        filter_args = StamperProfiles(cli_args)
 
-        elif args.slate_time == 0:      # there's no slate info from the CLI
-            jump = makeJump(args)
-            to_output = joinAndPostFilters(args, [jump])
+        if cli_args.stamp:      # the stamp flag is set, so do that...
+            framestamped = makeStamped(cli_args)
+            to_output = joinAndPostFilters(cli_args, [framestamped])
+
+        elif cli_args.slate_time == 0:      # there's no slate info from the CLI
+            main_jump = makeMainJump(cli_args)
+            to_output = joinAndPostFilters(cli_args, [main_jump])
 
         else:           # we have a slate to add up front...
-            slate = makeSlate(args)
-            jump = makeJump(args)
-            to_output = joinAndPostFilters(args, [slate, jump])
+            slate = makeSlate(cli_args)
+            main_jump = makeMainJump(cli_args)
+            to_output = joinAndPostFilters(cli_args, [slate, main_jump])
 
-        if args.encoder_prof == 'null':
-            args.output_file = '/dev/null'
-        output = ffmpeg.output(to_output[0], args.output_file, **output_args)
+        if cli_args.encoder_prof == 'null':
+            cli_args.output_file = '/dev/null'
 
+        output = ffmpeg.output(to_output[0], cli_args.output_file, **filter_args.output)
         output.run()
         print (f"FFMPEG command string was: \n {' '.join(output.compile())}")
 
